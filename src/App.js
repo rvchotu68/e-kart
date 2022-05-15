27813242f18130612956db318b56ad100ce01849
Aravind Kumar <arvind.arvindkumar1994@gmail.com>
2022-05-15 14:27:40 +0530

    landingPage

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import './App.css';
+
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+      <div className="category-body-container">
+      <h2>Hat</h2>
+      <p>Shop now</p>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
